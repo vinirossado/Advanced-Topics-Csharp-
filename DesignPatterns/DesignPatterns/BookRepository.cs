4c88219bad73322988e9b1be8aacfe9b1e46f3ba
Vinicius Rossado <vinirossado@gmail.com>
2022-01-25 19:13:43 -0300

    Fim do LambaExpression

---
+++ DesignPatterns/DesignPatterns/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "Book 1", Price = 5},
+                new Book() {Title = "Book 2", Price = 7},
+                new Book() {Title = "Book 3", Price = 19}
+            };
+        }
+
+    }
+}
--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,23 +1,32 @@
 ﻿using DesignPatterns.Delegates;
+using DesignPatterns.Generics;
 using System;
 
 namespace DesignPatterns
 {
     internal class Program
     {
+        #region Lamba Expression
+
+        //Lamba Expression
+        //    static void Main(string[] args)
+        //    {
+        //        var books = new BookRepository().GetBooks();
+        //        Action<Book> del;
+
+        //        var cheapBooks = books.FindAll(x => x.Price <= 10);
+
+        //        foreach (var book in cheapBooks)
+        //        {
+        //            Console.WriteLine(book.Title);
+        //        }
+        //    }
+        //}
+        #endregion Lamba Expression
+
         static void Main(string[] args)
         {
-            var processor = new PhotoProcessor();
-            var filters = new PhotoFilters();
-            Action<Photo> filterHandler = filters.ApplyContrast;
-            filterHandler += filters.ApplyBrightness;
-            filterHandler += RemoveRedEyeFilter;
-            processor.Process("", filterHandler);
-        }
 
-        static void RemoveRedEyeFilter(Photo photo)
-        {
-            Console.WriteLine("Apply RemoveRedEye");
         }
     }
 }
